6b79acdba248a4941fe7906afe4f28483123bbe3
Mohit Sharma <msvbs98@gmail.com>
2018-08-17 12:55:57 +0000

    On server

---
+++ server.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
+
+const app = express();
+
+const options = {
+  key: fs.readFileSync('./privkey.pem'),
+  cert: fs.readFileSync('./fullchain.pem')
+};
+
+const staticPath = path.join(__dirname, 'dist');
+app.use(express.static(staticPath));
+app.use(redirectToHTTPS([/localhost:(\d{4})/], [], 301));
+
+http.createServer(app).listen(80);
+https.createServer(options, app).listen(443);
