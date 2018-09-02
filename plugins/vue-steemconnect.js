bc7c8261863ac621b3c5bab278a7adb7987bb900
Knowledges <31616620+onozasi@users.noreply.github.com>
2018-09-02 23:54:25 +0100

    Update vue-steemconnect.js

--- plugins/vue-steemconnect.js
+++ plugins/vue-steemconnect.js
@@ -3,6 +3,6 @@ import VueSteemConnect from 'vue-steemconnect'
 
 Vue.use(VueSteemConnect, {
   app: 'knacksteem.app',
-  callbackURL: process.env.scRedirectUrl || 'https://knacksteem.org/callback',
+  callbackURL: process.env.scRedirectUrl || 'https://knacksteem.org/',
   scope: ['login', 'custom_json', 'claim_reward_balance', 'vote', 'comment', 'comment_options', 'delete_comment']
 })
