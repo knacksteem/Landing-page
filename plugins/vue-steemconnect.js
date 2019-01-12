4e41f0562b32afe22388d1b5b8bc96284704d678
Mohit Sharma <msvbs98@gmail.com>
2019-01-12 14:42:45 +0530

    Change Callback URL

--- plugins/vue-steemconnect.js
+++ plugins/vue-steemconnect.js
@@ -3,6 +3,6 @@ import VueSteemConnect from 'vue-steemconnect'
 
 Vue.use(VueSteemConnect, {
   app: 'knacksteem.app',
-  callbackURL: process.env.scRedirectUrl || 'https://knacksteem.org/',
+  callbackURL: process.env.scRedirectUrl || 'https://knacksteem.org/callback',
   scope: ['login', 'custom_json', 'claim_reward_balance', 'vote', 'comment', 'comment_options', 'delete_comment']
 })
